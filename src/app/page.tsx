73e94272ebf2a735dd644f5d5a25fed783b7e5f9
Matheus Emanuel <matheusemanuel745@gmail.com>
2023-11-21 12:29:45 -0300

    new stuff4
    Signed-off-by: Matheus Emanuel <matheusemanuel745@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,7 +35,7 @@ export default function Home() {
     abi: abi,
     functionName: "mintWithRewards",
     chainId: CHAIN_ID,
-    args: [isConnected, 1, "", mintReferral],
+    args: [address, 1, "", mintReferral],
     enabled: isConnected
   });
 
