ad8ac669c01addcf6d27b98d9cbc507f0d5536a6
Matheus Emanuel <matheusemanuel745@gmail.com>
2023-11-22 16:22:37 -0300

    stufff
    Signed-off-by: Matheus Emanuel <matheusemanuel745@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -99,7 +99,7 @@ export default function Home() {
       <Header />
       <div className="flex justify-center items-center mt-4 mb-6">
         <p className="text-base text-center text-justify text-white w-[32rem]">
-          Choose a button and mint a "free" random NFT on the Zora Network.
+          Choose a button and mint a "free" random NFT on the Zora Network!!!!
         </p>
       </div>
       <div className="flex justify-center items-center bg-black">
